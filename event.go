efd3a91b4cb1c2e9d584615e3a4dab6e65f93e46
Andrew Schreiber <schreiaj@gmail.com>
2015-10-23 17:14:54 -0400

    ran gofmt.

--- event.go
+++ event.go
@@ -1,31 +1,31 @@
 package tba
 
 import (
-  "os"
-  "net/http"
-  "io/ioutil"
-  "encoding/json"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
-type Event struct{
-  Key string `json:"key"`
-  Name string `json:"name"`
-  Short_Name string `json:"short_name"`
-  Event_Code string `json:"event_code"`
-  Official bool `json:"official"`
-  Year int `json:"year"`
-  Start_Date string `json:"start_date"`
+type Event struct {
+	Key        string `json:"key"`
+	Name       string `json:"name"`
+	Short_Name string `json:"short_name"`
+	Event_Code string `json:"event_code"`
+	Official   bool   `json:"official"`
+	Year       int    `json:"year"`
+	Start_Date string `json:"start_date"`
 }
 
-func LoadEvents(year string) []Event{
-  client := &http.Client{}
-  req, _ := http.NewRequest("GET", "http://www.thebluealliance.com/api/v2/events/" + year, nil)
-  req.Header.Set("X-TBA-App-Id", os.Getenv("TBA_KEY"))
-  res, _ := client.Do(req)
-  defer res.Body.Close()
-  body, _ := ioutil.ReadAll(res.Body)
+func LoadEvents(year string) []Event {
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "http://www.thebluealliance.com/api/v2/events/"+year, nil)
+	req.Header.Set("X-TBA-App-Id", os.Getenv("TBA_KEY"))
+	res, _ := client.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
 
-  events := []Event{}
-  json.Unmarshal(body, &events)
-  return events
+	events := []Event{}
+	json.Unmarshal(body, &events)
+	return events
 }
--- match.go
+++ match.go
@@ -1,38 +1,53 @@
 package tba
 
 import (
-  "net/http"
-  "io/ioutil"
-  "encoding/json"
-  "os"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
-type Match struct{
-  Key string `json:"key"`
-  Level string `json:"comp_level"`
-  Participants Participants `json:"alliances"`
+type Match struct {
+	Key          string       `json:"key"`
+	Level        string       `json:"comp_level"`
+	Participants Participants `json:"alliances"`
 }
 
-
-type Participants struct{
-  BlueAlliance Alliance `json:"blue"`
-  RedAlliance Alliance `json:"red"`
+type Participants struct {
+	BlueAlliance Alliance `json:"blue"`
+	RedAlliance  Alliance `json:"red"`
 }
 
-type Alliance struct{
-  Score int `json:"score"`
-  Teams []string `json:"teams"`
+type Alliance struct {
+	Score int      `json:"score"`
+	Teams []string `json:"teams"`
 }
 
-func LoadMatches(eventKey string) []Match{
-  client := &http.Client{}
-  req, _ := http.NewRequest("GET", "http://www.thebluealliance.com/api/v2/event/" + eventKey + "/matches", nil)
-  req.Header.Set("X-TBA-App-Id", os.Getenv("TBA_KEY"))
-  res, _ := client.Do(req)
-  defer res.Body.Close()
-  body, _ := ioutil.ReadAll(res.Body)
+func LoadMatches(eventKey string) []Match {
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "http://www.thebluealliance.com/api/v2/event/"+eventKey+"/matches", nil)
+	req.Header.Set("X-TBA-App-Id", os.Getenv("TBA_KEY"))
+	res, _ := client.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
 
-  matches := []Match{}
-  json.Unmarshal(body, &matches)
-  return matches
+	matches := []Match{}
+	json.Unmarshal(body, &matches)
+	return matches
+}
+
+func GetTeams(match Match) []string {
+	teams := append([]string(nil), match.Participants.RedAlliance.Teams...)
+	teams = append(teams, match.Participants.BlueAlliance.Teams...)
+	return teams
+}
+
+func GetWinningTeams(match Match) []string {
+	if match.Participants.RedAlliance.Score > match.Participants.BlueAlliance.Score {
+		return match.Participants.RedAlliance.Teams
+	}
+	if match.Participants.RedAlliance.Score < match.Participants.BlueAlliance.Score {
+		return match.Participants.BlueAlliance.Teams
+	}
+	return []string(nil)
 }
